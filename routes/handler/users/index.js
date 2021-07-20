609da721667a5c5629ab39861a4c7dbec53996b2
Alif Irfandi <alifirfandi79@gmail.com>
2021-07-20 22:20:27 +0700

    get & update user

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,7 +1,13 @@
 const register = require("./register");
 const login = require("./login");
+const update = require("./update");
+const getUsers = require("./getusers");
+const getUser = require("./getUser");
 
 module.exports = {
   register,
   login,
+  update,
+  getUsers,
+  getUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const usersHandler = require("./handler/users");
 
+router.get("/", usersHandler.getUsers);
+router.get("/:id", usersHandler.getUser);
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
+router.put("/:id", usersHandler.update);
 
 module.exports = router;
