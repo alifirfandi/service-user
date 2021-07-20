e35a4c5519afedebce59b4392150b790d08e4e87
Alif Irfandi <alifirfandi79@gmail.com>
2021-07-20 10:32:42 +0700

    add models

---
+++ models/RefreshToken.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const RefreshToken = sequelize.define(
+    "RefreshToken",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        field: "user_id",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "refresh-token",
+      timestamps: true,
+    }
+  );
+
+  return RefreshToken;
+};
